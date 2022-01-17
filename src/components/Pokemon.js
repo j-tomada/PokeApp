20529284d5268548cfb98a7e14fbd4424af6b683
Joseph Tomada <josephtomada@gmail.com>
2022-01-16 23:24:47 -0800

    Updated search for modern features. Created onSuggestHandler which will be used to fetch API from chosen pokemon

--- src/components/Pokemon.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Pokemon extends Component {
-
-    render() {
-        return <h1>Class Component</h1>
-        let variable = Document.getEl
-    }
-}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,61 @@
 import React, { useState } from 'react'
-import IconButton from '@mui/material/IconButton';
 import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
+import CloseTwoToneIcon from '@mui/icons-material/CloseTwoTone';
 import './SearchBar.css'
 
 export default function SearchBar ({ placeholder, data }) {
     const [filteredData, setFilteredData] = useState([])
+    const [wordEntered, setWordEntered] = useState("")
 
     const handleFilter = (event) => {
         const searchWord = event.target.value;
+        setWordEntered(searchWord)
         const newFilter = data.filter((value) => {
             return value.name.includes(searchWord);
          });
-         setFilteredData(newFilter);
+
+         if (searchWord ==="") {
+             setFilteredData([])
+         }
+         else {
+             setFilteredData(newFilter)
+         }
     }    
+
+    const onSuggestHandler = (text) => {
+        setWordEntered(text)
+        setFilteredData([])
+    }
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("")
+
+    }
     return (
         <div className='search'>
             <div className='searchInputs'>
-                <input type='text' placeholder={placeholder} onChange={handleFilter} className='inputBtn'/>
+                <input 
+                    type='text' 
+                    placeholder={placeholder}
+                    value={wordEntered} 
+                    onChange={handleFilter} 
+                    className='inputBtn'
+                 />
                 <div className='searchIcon'>
-                    <SearchTwoToneIcon/>
+                    {filteredData.length === 0 ? <SearchTwoToneIcon /> : <CloseTwoToneIcon  id='clearBtn' onClick={clearInput}/>}
                 </div>
             </div>
             {filteredData.length !== 0 && (
             <div className='dataResult'>
-                {filteredData.slice(0, 15).map((value, key) => {
+                {filteredData.map((value, key) => {
                     return ( 
-                        <a className='dataItem' href={value.url} target="_blank">
-                            <p>{value.name}</p>
+                        <a className='dataItem' target="_blank" key={key}>
+                            <p onClick={() =>  {
+                                onSuggestHandler(value.name);
+                                console.log(value.name);
+                            }}
+                            >{value.name}</p>
                         </a>
                     );
                 })}
